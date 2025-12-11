e1b39c5ad77be640535b727946edd0d344b6db96
Rob Aparri <32496022+raparri01@users.noreply.github.com>
2025-12-11 17:34:53 -0500

    fix drift type error

--- src/utils/drift.ts
+++ src/utils/drift.ts
@@ -1,10 +1,10 @@
-import { createDrift, Drift } from "@delvtech/drift";
+import { createDrift, Drift, ReadWriteAdapter } from "@delvtech/drift";
 import { viemAdapter } from "@delvtech/drift-viem";
 import { getPublicClient } from "@wagmi/core";
 import { config } from "../lib/wagmi";
 import { WalletClient } from "viem";
 
-export function getDrift(walletClient?: WalletClient): Drift {
+export function getDrift(walletClient?: WalletClient): Drift<ReadWriteAdapter> {
   const publicClient = getPublicClient(config);
   // @ts-ignore
   return createDrift({ adapter: viemAdapter({ publicClient, walletClient }) });
